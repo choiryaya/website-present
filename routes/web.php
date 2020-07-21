891d9e0e40d03aec4b2351044bc15885126e844e
unknown <yessypermatasari8@gmail.com>
2020-07-21 09:38:12 +0700

    add login template

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('admin.template.login');
+});
+Route::get('home', function () {
     return view('admin.template.default');
 });
